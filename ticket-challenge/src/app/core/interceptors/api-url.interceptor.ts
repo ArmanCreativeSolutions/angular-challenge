eea8efeefe0c33cd8187d54f43d5209e6401583e
Milaad <mrmilad.moradi@gmail.com>
2022-06-03 20:48:40 +0430

    [feat] implement interceptor

---
+++ ticket-challenge/src/app/core/interceptors/api-url.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment} from "../../../environments/environment";
+
+@Injectable()
+export class ApiUrlInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    request = request.clone({ url: environment.apiBaseUrl + request.url });
+    return next.handle(request);
+  }
+}
