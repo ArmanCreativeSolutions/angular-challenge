dba85e317b4c75c49c5cc593d263446de5c75585
Milaad <mrmilad.moradi@gmail.com>
2022-06-03 21:10:30 +0430

    [fix] change route and implement lazyload

--- ticket-challenge/src/app/app-routing.module.ts
+++ ticket-challenge/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'salons',
+    pathMatch: 'full'
+  },
+  {
+    path: 'salons',
+    loadChildren: () => import('./salons-list/salons-list.module').then((m) => m.SalonsListModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ticket-challenge/src/app/app.module.ts
+++ ticket-challenge/src/app/app.module.ts
@@ -3,21 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlanComponent } from './plan/plan.component';
-import { SalonsListComponent } from './salons-list/salons-list.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {ApiUrlInterceptor} from "./core/interceptors/api-url.interceptor";
 
 @NgModule({
-    declarations: [AppComponent, PlanComponent, SalonsListComponent],
-    imports: [BrowserModule, AppRoutingModule],
-    providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: ApiUrlInterceptor,
-            multi: true,
-        },
-    ],
-    bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ApiUrlInterceptor, multi: true}
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
